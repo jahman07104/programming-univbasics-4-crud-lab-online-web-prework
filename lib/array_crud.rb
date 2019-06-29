c7cbf37c5644da67d40fe54acb3dbbc9ad518df0
Patrick Harrison <jahman@bellatlantic.net>
2019-06-29 20:19:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,6 +1,5 @@
 def create_an_empty_array
-   
-  []
+  my_array=[] 
 end
 
 def create_an_array
@@ -16,15 +15,15 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop
+  element=array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
-endelement.shift
+  element=array.shift
+end
 
 def retrieve_element_from_index(array, index_number)
-  array[]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
@@ -32,9 +31,9 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  array[3]
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-array[]element
+array[2]=element
 end
